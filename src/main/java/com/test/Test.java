d9cd7257976fcae6423818a89a0f36d36b97e867
xiaoss <xssjava@163.com>
2021-04-21 07:39:49 +0800

    qdd

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -3,6 +3,6 @@ package com.test;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("11111111111111111111");
+        System.out.println("22222222222222222222222222222222");
     }
 }
